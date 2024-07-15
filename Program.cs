c5d96f861135957f9a1a142d3b45ef860de0484e
Jatin Kumar <Jatin.Kumar@asl.mum.sst>
2024-07-15 20:39:11 +0530

    Initial Commit

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Enum
+{
+    enum months
+    {
+        January,
+        February,
+        March,
+        April,
+        May,
+        June,
+        July,
+        August,
+        September,
+        October,
+        November,
+        December
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine($"Current month is : {months.July}");
+            Console.WriteLine($"It is the {(int)months.July+1}th month of the year 2024");
+        }
+    }
+}
